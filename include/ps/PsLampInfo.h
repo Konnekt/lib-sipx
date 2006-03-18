d75f1f56dad7d846b48c07e2d3bfd9b11df3fa65
Rafal Lindemann <rl@stamina.pl>
2006-03-18 21:58:48 +0000

    SipXTAPI 3.3 - wpis licencji (zamieniony na jeden standardowy, dla niektórych plików pewnie się zmieni...)
    git-svn-id: http://www.konnekt.info/svn/sipx/branches/sipxtapi_3.3/sipXcallLib@61 bbff28f8-6117-1d4f-bc34-7e17a506d0fd

--- include/ps/PsLampInfo.h
+++ include/ps/PsLampInfo.h
@@ -1,10 +1,12 @@
 //
-// Copyright (C) 2004, 2005 Pingtel Corp.
-// 
+// Copyright (C) 2004-2006 SIPfoundry Inc.
+// Licensed by SIPfoundry under the LGPL license.
+//
+// Copyright (C) 2004-2006 Pingtel Corp.  All rights reserved.
+// Licensed to SIPfoundry under a Contributor Agreement.
 //
 // $$
-////////////////////////////////////////////////////////////////////////
-//////
+///////////////////////////////////////////////////////////////////////////////
 
 #ifndef _PsLampInfo_h_
 #define _PsLampInfo_h_
