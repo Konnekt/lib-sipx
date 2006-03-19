3a37c7c14f69ebacac101e46197a8e73e11518ff
Rafal Lindemann <rl@stamina.pl>
2006-03-19 14:02:36 +0000

    Program testujący
    git-svn-id: http://www.konnekt.info/svn/sipx/branches/sipxtapi_3.3@81 bbff28f8-6117-1d4f-bc34-7e17a506d0fd

--- sipXportLib/src/test/sipxunit/unitmain.cpp
+++ sipXportLib/src/test/sipxunit/unitmain.cpp
@@ -10,7 +10,10 @@
 
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <sipxunit/TestRunner.h>
-
+#include <conio.h>
+#include <io.h>
+#include <stdio.h>
+#include <os/OsSysLog.h>
 /**
  * include this in your project and it will find all Suite's that have been
  * registered with the CppUnit TextFactoryRegistry and run them
@@ -18,9 +21,18 @@
 int main( int argc, char* argv[] )
 {
     TestRunner runner;
+	OsSysLog::initialize(0, // do not cache any log messages in memory
+                        "sipXtest"); // name for messages from this program
+	OsSysLog::enableConsoleOutput(false);
+	unlink("sipxtest.log");
+	OsSysLog::setOutputFile(0, "sipxtest.log");
     CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+	OsSysLog::setLoggingPriority(PRI_DEBUG) ;
     runner.addTest(registry.makeTest());
     bool wasSucessful = runner.run();
+
+	printf("Press anuthong to continue...");
+	getch();
 
     return !wasSucessful;
 }
